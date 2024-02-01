475a8a79cd66d73d22e5cc879657119d8162dd6f
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-01 01:10:31 -0300

    Agregamos el mensaje utilizando el useState

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,9 +1,14 @@
-function Titulo({mensaje}) {
+import { useState } from "react";
 
-    return (
-      <h1>Hello {mensaje}!</h1>
-    )
+function Titulo({ mensaje }) {
+  const [texto, setTexto] = useState("");
+
+  return (
+    <>
+      <h1>Hello {mensaje + texto}</h1>
+      <button onClick={() => setTexto(' (from changed state)')}>Click me!</button>
+    </>
+  );
 }
-  
-export default Titulo
-  
+
+export default Titulo;
